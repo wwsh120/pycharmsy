87564edbfcd944d34ce0d8f5652560af5259000c
DESKTOP-VCLL26H\KingGOD <wwjh12079@gmail.com>
2020-10-19 18:02:12 +0900

    Initial commit

---
+++ packmman.py
@@ -0,0 +1,123 @@
+import pygame, sys, random
+from pygame.locals import *
+
+
+pygame.init()
+clock = pygame.time.Clock()
+pygame.display.set_caption("PACKMAN")
+screen = pygame.display.set_mode((1920, 1080))
+
+
+block_image = pygame.image.load("C:\\Users\\KingGOD\\Desktop\\hangman\\ww.jpg").convert()
+ghost_image = pygame.image.load("C:\\Users\\KingGOD\\Desktop\\hangman\\gho1.png").convert()
+coin_image = pygame.image.load("C:\\Users\\KingGOD\\Desktop\\hangman\\cccc.png").convert()
+
+
+
+class block:
+    def __init__(self, x, y):
+        self.x = x * 50
+        self.y = y * 50
+        self.sort = int(a[y][x])
+
+    def draw(self):
+        if self.sort == 0:
+            screen.blit(coin_image, (self.x, self.y))
+        elif self.sort == 1:
+            screen.blit(block_image, (self.x, self.y))
+
+class player:
+    def __init__(self):
+        self.x = random.randint(5 ,10) * 50
+        self.y = random.randint(7 ,12) * 50
+        self.point = 0
+
+    def draw(self):
+        screen.blit(ghost_image, (self.x, self.y))
+
+    def move(self):
+        if pressed_keys[K_RIGHT] and (self.x_key != (2 or 3)):
+            self.x += 2
+        if pressed_keys[K_LEFT] and (self.x_key != (1 or 3)):
+            self.x -= 2
+        if pressed_keys[K_UP] and (self.y_key != (2 or 3)):
+            self.y -= 2
+        if pressed_keys[K_DOWN] and (self.y_key != (1 or 3)):
+            self.y += 2
+
+    def positioning(self):
+        self.x_key = 0
+        self.y_key = 0
+
+        if a[int(self.y / 50)][int(self.x / 50)] == 0:
+                self.point += 1
+        if a[int(self.y // 50)][int(round(self.x / 50))] == 1:
+                self.y_key = 2
+        if a[int(self.y // 50) + 1][int(round(self.x / 50))] == 1:
+            if self.y_key == 2:
+                 self.y_key = 3
+            else:
+                 self.y_key = 1
+        if a[int(round(self.y / 50))][int(self.x // 50) + 1] == 1:
+            self.x_key = 2
+        if a[int(round(self.y / 50))][int(self.x // 50)] == 1:
+            if self.x_key == 2:
+                self.x_key = 3
+            else:
+                self.x_key = 1
+
+
+
+
+
+
+
+a = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ,[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ,[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ,[1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ,[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ,[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ,[1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ,[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ,[1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ,[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ,[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ,[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ,[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ,[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ,[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ,[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ,[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ,[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ,[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ,[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ,[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+blocks = []
+
+player = player()
+
+for y in range(21):
+    for x in range(38):
+        blocks.append(block(x, y))
+
+while 1:
+    clock.tick(144)
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            sys.exit()
+
+    pressed_keys = pygame.key.get_pressed()
+    screen.fill((0, 0, 0))
+
+    for i in range(len(blocks)):
+        blocks[i].draw()
+
+    player.draw()
+    player.move()
+    player.positioning()
+
+
+
+    pygame.display.update()
